bbcb5c84fdb3ddbbec9de50e4057a8d892db0678
nwogburumichael <michael.nwogburu.junior@gmail.com>
2021-10-19 17:19:02 +0200

    Code led avec bouton

--- Tp_bouton_git/src/main.cpp
+++ Tp_bouton_git/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
+const int led = 13;
+const int bouton = 12 ;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(bouton,INPUT);
+  pinMode(led ,OUTPUT);
+  Serial.begin(9600);
+  int etat_bouton = 0;
 }
 
 void loop() {
+
+  int etat_bouton = digitalRead(bouton);
+  //Serial.println(etat_bouton);
+  //delay(1000);
   // put your main code here, to run repeatedly:
+if (etat_bouton == HIGH)
+   {digitalWrite(led,HIGH);}
+else
+{digitalWrite(led,LOW);}
 }
